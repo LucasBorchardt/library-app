c7d9229572480007a4a0dccecf0b3b8b28f6b18b
John Doe <lucas.lopez.borchardt@gmail.com>
2023-03-27 14:19:42 +0200

    testing

--- src/pages/userPage/[id].js
+++ src/pages/userPage/[id].js
@@ -1,10 +1,13 @@
 import { useState, useEffect } from "react";
 import Link from "next/link";
 import { useSession, signOut } from "next-auth/react";
+import Layout from "@/components/Layout";
 
 export default function UserPage() {
   const { data: session, status } = useSession({required: true});
   const [books, setBooks] = useState([]);
 
-
+return (
+  <Layout/>
+)
 }
