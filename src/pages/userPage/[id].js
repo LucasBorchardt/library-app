576eeb90de227a7b44f9d1fc25b1e1f00b20ec40
John Doe <lucas.lopez.borchardt@gmail.com>
2023-03-27 12:39:58 +0200

    UserPage name corrected

--- src/pages/userPage/[id].js
+++ src/pages/userPage/[id].js
@@ -2,30 +2,9 @@ import { useState, useEffect } from "react";
 import Link from "next/link";
 import { useSession, signOut } from "next-auth/react";
 
-export default function userPage() {
-  const { data: session, status } = useSession();
+export default function UserPage() {
+  const { data: session, status } = useSession({required: true});
   const [books, setBooks] = useState([]);
 
-  useEffect(() => {
-    async function getBooks() {
-      try {
-        const booksData = await fetch("/api/books");
-        const books = await booksData.json();
-        setBooks(books);
-      } catch (error) {
-        console.log(error);
-      }
-    }
-    getBooks();
-  }, []);
 
-  const filteredBooks = books.filter((book) => {
-    if (session?.user.id === book.userId) {
-      return book;
-    }
-  });
-
-  if (status === "authenticated") {
-    return 
-}
 }
